99f1a6ba9507305685386a3fa321bd1c2e0eccdc
Sebastian Alonzo <thekyuu@gmail.com>
2025-10-10 23:49:50 +0200

    hidden map scroll bug fix

--- Scenes/UI/Map/map.gd
+++ Scenes/UI/Map/map.gd
@@ -20,6 +20,9 @@ func _ready() -> void:
 	camera_edge_y = MapGenerator.Y_DIST * (MapGenerator.ROWS - 1)
 
 func _input(event: InputEvent) -> void:
+	if not visible:
+		return
+	
 	if event.is_action_pressed("scroll_up"):
 		camera_2d.position.y -= SCROLL_SPEED
 	elif event.is_action_pressed("scroll_down"):
